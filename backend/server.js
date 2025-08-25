6ba486487b804ba84f3f4f28171ae0237b078aa8
Rashel Akhtar <akhtarrashel408@gmail.com>
2025-08-25 17:34:26 +0530

    change port

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.WEB_PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Allow requests from frontend
